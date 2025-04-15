8a8bd652d010a5029143dbc42d0e4b37bdc10a22
𝔍𝔲𝔩𝔦𝔢𝔫 𝔐𝔦𝔩𝔬 𝔒𝔯𝔱𝔢𝔤𝔞 <julienmilo369@gmail.com>
2025-04-15 21:21:57 +0200

    fix: contact model(avatar)

--- server/src/resources/contact/contact.model.js
+++ server/src/resources/contact/contact.model.js
@@ -1,10 +1,11 @@
 export class Contact {
-    constructor({ id, firstName, lastName, email, phone, address}) {
+    constructor({ id, firstName, lastName, email, phone, address, avatar }) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phone = phone;
         this.address = address;
+        this.avatar = avatar || null;
     }
 }
